dedd3903ebebc40dd06abe0fda8e48a126bd84b3
mahmoud farid <mahmoud.farid.94@gmail.com>
2018-04-19 15:41:28 +0200

    Remove unused templates, views and urls

--- articles/users/models.py
+++ articles/users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -12,6 +11,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-
-    def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
